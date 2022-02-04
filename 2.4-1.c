8981368d7f00527452812109eb9bff6e8c4b3761
pHonglei <97824987+pHonglei@users.noreply.github.com>
2022-02-04 16:10:16 +0800

    Add files via upload

---
+++ 2.4-1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=0;
+    int i=0;
+    while(i<=100){
+        if (a%2==1){
+            printf("%d\n",a);
+           
+        }
+        a++;
+        i++;
+    }
+return 0;
+}
